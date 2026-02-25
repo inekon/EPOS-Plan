93d9b048d5797a92abd7f1882bcd5f0a5a73b826
wgnode <wilfried.goetz@gmx.de>
2026-02-25 20:41:19 +0100

    add

--- WindowsFormsApplication1/Views/Klimadaten/Klimadaten.Designer.cs
+++ WindowsFormsApplication1/Views/Klimadaten/Klimadaten.Designer.cs
@@ -28,16 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.listBoxKlimreg = new System.Windows.Forms.ListBox();
             this.butt_Delete = new System.Windows.Forms.Button();
             this.btn_Beenden = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.pBar_Import = new System.Windows.Forms.ProgressBar();
             this.btn_Import = new System.Windows.Forms.Button();
             this.textBox_Display = new System.Windows.Forms.TextBox();
@@ -49,8 +51,17 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // listBoxKlimreg
@@ -58,7 +69,7 @@
             this.listBoxKlimreg.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.listBoxKlimreg.FormattingEnabled = true;
             this.listBoxKlimreg.ItemHeight = 17;
-            this.listBoxKlimreg.Location = new System.Drawing.Point(21, 259);
+            this.listBoxKlimreg.Location = new System.Drawing.Point(21, 235);
             this.listBoxKlimreg.Margin = new System.Windows.Forms.Padding(4);
             this.listBoxKlimreg.Name = "listBoxKlimreg";
             this.listBoxKlimreg.Size = new System.Drawing.Size(200, 225);
@@ -69,7 +80,7 @@
             // butt_Delete
             // 
             this.butt_Delete.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.butt_Delete.Location = new System.Drawing.Point(21, 492);
+            this.butt_Delete.Location = new System.Drawing.Point(21, 464);
             this.butt_Delete.Margin = new System.Windows.Forms.Padding(4);
             this.butt_Delete.Name = "butt_Delete";
             this.butt_Delete.Size = new System.Drawing.Size(98, 30);
@@ -83,7 +94,7 @@
             // 
             this.btn_Beenden.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btn_Beenden.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.btn_Beenden.Location = new System.Drawing.Point(630, 536);
+            this.btn_Beenden.Location = new System.Drawing.Point(640, 536);
             this.btn_Beenden.Margin = new System.Windows.Forms.Padding(4);
             this.btn_Beenden.Name = "btn_Beenden";
             this.btn_Beenden.Size = new System.Drawing.Size(98, 28);
@@ -109,7 +120,7 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(738, 28);
+            this.label1.Size = new System.Drawing.Size(748, 28);
             this.label1.TabIndex = 13;
             this.label1.Text = "Importieren Sie hier die meteorologische Datensätze (TMY)  für die Klimaregion";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -117,41 +128,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 236);
+            this.label2.Location = new System.Drawing.Point(17, 212);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(202, 19);
             this.label2.TabIndex = 15;
             this.label2.Text = "Liste der importierten Regionen";
-            // 
-            // chart1
-            // 
-            chartArea4.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
-            chartArea4.AxisX.Title = "Tage";
-            chartArea4.AxisY.Title = "Temperatur [°C]";
-            chartArea4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(255)))), ((int)(((byte)(222)))));
-            chartArea4.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
-            chartArea4.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            this.chart1.Location = new System.Drawing.Point(245, 241);
-            this.chart1.Name = "chart1";
-            series4.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.IsVisibleInLegend = false;
-            series4.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
-            series4.Name = "Series1";
-            series4.SmartLabelStyle.Enabled = false;
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(481, 282);
-            this.chart1.TabIndex = 16;
-            this.chart1.TabStop = false;
-            this.chart1.Text = "chart1";
-            title4.Name = "Jahrestemperatur Ganglinie";
-            title4.Text = "Jahrestemperatur Ganglinie";
-            this.chart1.Titles.Add(title4);
-            this.chart1.AxisViewChanged += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ViewEventArgs>(this.chart1_AxisViewChanged);
-            this.chart1.AxisScrollBarClicked += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ScrollBarEventArgs>(this.AxisScrollBarClicked);
             // 
             // pBar_Import
             // 
@@ -272,13 +253,103 @@
             this.groupBox1.TabIndex = 29;
             this.groupBox1.TabStop = false;
             // 
+            // chart1
+            // 
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.AxisX.Title = "Tage";
+            chartArea1.AxisY.Title = "Temperatur [°C]";
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(255)))), ((int)(((byte)(222)))));
+            chartArea1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            chartArea1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(6, 6);
+            this.chart1.Name = "chart1";
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series1.Name = "Series1";
+            series1.SmartLabelStyle.Enabled = false;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(481, 271);
+            this.chart1.TabIndex = 16;
+            this.chart1.TabStop = false;
+            this.chart1.Text = "chart1";
+            title1.Name = "Jahrestemperatur Ganglinie";
+            title1.Text = "Jahrestemperatur Ganglinie";
+            this.chart1.Titles.Add(title1);
+            this.chart1.AxisViewChanged += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ViewEventArgs>(this.chart1_AxisViewChanged);
+            this.chart1.AxisScrollBarClicked += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ScrollBarEventArgs>(this.AxisScrollBarClicked);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(229, 216);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(509, 313);
+            this.tabControl1.TabIndex = 30;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(501, 283);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Temperatur";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(501, 283);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sonnenwinkel";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart2
+            // 
+            chartArea2.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea2.AxisX.Title = "Monat";
+            chartArea2.AxisY.Title = "Sonnenwinkel [°]";
+            chartArea2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(255)))), ((int)(((byte)(222)))));
+            chartArea2.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            chartArea2.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(10, 6);
+            this.chart2.Name = "chart2";
+            series2.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsVisibleInLegend = false;
+            series2.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series2.Name = "Series1";
+            series2.SmartLabelStyle.Enabled = false;
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(481, 271);
+            this.chart2.TabIndex = 17;
+            this.chart2.TabStop = false;
+            this.chart2.Text = "chart2";
+            title2.Name = "Jahresverlauf Sonnenwinkel";
+            title2.Text = "Jahresverlauf Sonnenwinkel";
+            this.chart2.Titles.Add(title2);
+            // 
             // Form_Klimadaten
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(738, 571);
+            this.ClientSize = new System.Drawing.Size(748, 571);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.chart1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
@@ -292,9 +363,13 @@
             this.Name = "Form_Klimadaten";
             this.Text = "Klimadaten";
             this.Load += new System.EventHandler(this.Form_Klimadaten_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -308,7 +383,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.ProgressBar pBar_Import;
         private System.Windows.Forms.Button btn_Import;
         private System.Windows.Forms.TextBox textBox_Display;
@@ -320,5 +394,10 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
     }
 }
