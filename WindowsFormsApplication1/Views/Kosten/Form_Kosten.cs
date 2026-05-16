0797b725629efa72e691a7d38456b56adab577e9
inekon <info@inekon.de>
2026-05-16 12:29:22 +0200

    add

--- WindowsFormsApplication1/Views/Kosten/Form_Kosten.cs
+++ WindowsFormsApplication1/Views/Kosten/Form_Kosten.cs
@@ -808,6 +808,7 @@ namespace WindowsFormsApplication1
                     case "LIQUID_FUEL":
                     case "SOLID_FUEL":
                     case "ANIMAL_FAT":
+                    case "HEAT":
                         uc = new ucFuelSettings(m_ID_Projekt, selectedCarrier);
                         break;
 
@@ -907,7 +908,7 @@ namespace WindowsFormsApplication1
                     try
                     {
                         DataRepository.ExecuteNonQuery(insertSql, ps);
-                        MessageBox.Show("Brennstoff-Variante erfolgreich angelegt.");
+                        MessageBox.Show("Energieträgervariante erfolgreich angelegt.");
                         return dlg.SelectedName;
                     }
                     catch (Exception ex)
